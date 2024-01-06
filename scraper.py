ea06999ecd0572c71e6da36c1854328ceed14b03
Soufyan <tameur.merad@gmail.com>
2024-01-06 18:48:50 +0100

    scrapper finished

--- scraper.py
+++ scraper.py
@@ -8,13 +8,53 @@ date = input("Enter Date following this format MM/DD/YYYY : ")
 
 
 page = requests.get(f"https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA?date={date}")
+matches_infos = []
+
 
 def main(page):
     src = page.content
-    # print(src)
     soup = BeautifulSoup(src, "lxml")
-    # print(soup)
-    championship = soup.find_all("div", {"class": "matchCard"})
-    print(championship)
+    championships = soup.find_all("div", {"class": "matchCard"})
+    championships_number = len(championships)
     
-main(page)
+    for i in range(championships_number):
+        championship_name = championships[i].contents[1].find("h2").text.strip()
+        matches = championships[i].find_all("div", {"class": "liItem"})
+        
+        matches_number = len(matches)
+        # print(matches_number)
+        
+        
+        # print(championship_name)
+        
+        
+        for j in range(matches_number):
+            teamA = matches[j].find('div', {'class': 'teamsData'}).find('div', {'class': 'teamA'}).text.strip()
+            # print(teamA)
+            teamB = matches[j].find('div', {'class': 'teamsData'}).find('div', {'class': 'teamB'}).text.strip()
+            
+            score = matches[j].find('div', {'class': 'teamsData'}).find('div', {'class': 'MResult'}).find_all("span",{'class': 'score'})
+            scoreA = score[0].text.strip()
+            scoreB = score[1].text.strip()
+            
+            time = matches[j].find('div', {'class': 'teamsData'}).find('div', {'class': 'MResult'}).find('span',{'class': 'time'}).text.strip()
+            match_info = {
+                "championship": championship_name,
+                "teamA": teamA,
+                "teamB": teamB,
+                "scoreA": scoreA,
+                "scoreB": scoreB,
+                "time": time
+            }
+            matches_infos.append(match_info)
+            
+
+
+            
+    
+            
+        
+    
+    
+        
+main(page)    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+        # print(championship_name)
